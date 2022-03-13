43d9ddb14f77e2a5af350d68b0dca8c93ff3ba3c
shahery <shaheryar924@gmail.com>
2022-03-13 17:54:14 +0000

    change variables name

--- run.py
+++ run.py
@@ -4,26 +4,26 @@ from random import randint
 # '' for available space
 # x for missing turns
 
-hidden_board = [['']*6 for x in range(6)]
-guess_board = [['']*6 for x in range(6)]
+hidden_plank = [['']*6 for x in range(6)]
+guess_plank = [['']*6 for x in range(6)]
 letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4,
                       'f': 5}
 
 
-def print_board(board):
+def print_plank(plank):
     print('a b c d e f')
     row_number = 1
-    for row in board:
+    for row in plank:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
 
-def create_ships(board):
+def create_ships(plank):
     for ship in range(5):
         ship_row, ship_column = randint(0, 5), randint(0, 5)
-    while board[ship_row][ship_column] == 'k':
+    while plank[ship_row][ship_column] == 'k':
         ship_row, ship_column = randint(0, 5), randint(0, 5)
-    board[ship_row][ship_column] = 'k'
+    plank[ship_row][ship_column] = 'k'
 
 
 def get_ship_location():
@@ -38,33 +38,33 @@ def get_ship_location():
     return(int(row)-1, letters_to_numbers[column])
 
 
-def count_hit_ships(board):
+def count_hit_ships(plank):
     count = 0
-    for row in board:
+    for row in plank:
         for column in row:
             if column == 'k':
                 count += 1
     return count
 
 
-create_ships(hidden_board)
-print(hidden_board)
+create_ships(hidden_plank)
+print(hidden_plank)
 turns = 5
 name = input('Please enter your name: ')
 print(f"Hello {name} welcome to battleship")
 while turns > 0:
-    print_board(guess_board)
+    print_plank(guess_plank)
     row, column = get_ship_location()
-    if guess_board[row][column] == 'x':
+    if guess_plank[row][column] == 'x':
         print('You already guessed that')
-    elif hidden_board[row][column] == 'k':
+    elif hidden_plank[row][column] == 'k':
         print('Congratulations, You have hit the battleship')
         print('Play again :)')
-        guess_board[row][column] = 'k'
+        guess_plank[row][column] = 'k'
         break
     else:
         print('Sorry, You missed')
-        guess_board[row][column] = 'x'
+        guess_plank[row][column] = 'x'
         turns -= 1
     print('You have ' + str(turns) + ' turns remaining')
     if turns == 0:
