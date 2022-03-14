1d9753cef168bb796c47f96905e40ffb8fcc3e41
shahery <shaheryar924@gmail.com>
2022-03-14 12:00:25 +0000

    Error solving

--- run.py
+++ run.py
@@ -11,34 +11,36 @@ letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4,
 
 
 def print_plank(plank):
-    print('a b c d e f')
+    print(' a b c d e f')
+    print(' -----------')
     row_number = 1
     for row in plank:
-        print("%d|%s|" % (row_number, "|".join(row)))
+        print("%d|%s|" % (row_number,  "|".join(row)))
         row_number += 1
 
 
-def create_ships(plank):
-    for ship in range(5):
-        ship_row, ship_column = randint(0, 5), randint(0, 5)
-    while plank[ship_row][ship_column] == 'k':
-        ship_row, ship_column = randint(0, 5), randint(0, 5)
-    plank[ship_row][ship_column] = 'k'
+def create_boats(plank):
+    for boat in range(5):
+        boat_row, boat_column = randint(0, 5), randint(0, 5)
+    while plank[boat_row][boat_column] == 'k':
+        boat_row, boat_column = randint(0, 5), randint(0, 5)
+    plank[boat_row][boat_column] = 'k'
 
 
-def get_ship_location():
-    row = input('Please enter a ship row 1-6: ')
+def get_boat_location():
+    print(' -----------')
+    row = input('Please enter a boat row 1-6: ')
     while row not in '123456':
         print('Please enter a vaild row')
-        row = input('Please  enter a ship row 1-6: ')
-    column = input('Please enter a ship column a-f: ')
+        row = input('Please  enter a boat row 1-6: ')
+    column = input('Please enter a boat column a-f: ')
     while column not in 'abcdef':
         print('Please enter a valid column')
-        column = input('Please enter a ship column a-f: ')
+        column = input('Please enter a boat column a-f: ')
     return(int(row)-1, letters_to_numbers[column])
 
 
-def count_hit_ships(plank):
+def count_hit_boats(plank):
     count = 0
     for row in plank:
         for column in row:
@@ -47,30 +49,38 @@ def count_hit_ships(plank):
     return count
 
 
-create_ships(hidden_plank)
-print(hidden_plank)
-turns = 5
-name = input('Please enter your name: ')
-print(f"Hello {name} welcome to battleship")
-while turns > 0:
-    print_plank(guess_plank)
-    row, column = get_ship_location()
-    if guess_plank[row][column] == 'x':
-        print('You already guessed that')
-    elif hidden_plank[row][column] == 'k':
-        print('Congratulations, You have hit the battleship')
-        print('Play again :)')
-        guess_plank[row][column] = 'k'
-        break
-    else:
-        print('Sorry, You missed')
-        guess_plank[row][column] = 'x'
-        turns -= 1
-    print('You have ' + str(turns) + ' turns remaining')
-    if turns == 0:
-        print('Sorry, you ran out of turns, The game over')
-        break
+def main():
+    create_boats(hidden_plank)
+    print(hidden_plank)
+    turns = 5
+    print('-------------------------------')
+    while True:
+        try:
+            name = str(input('Please enter your name: '))
+            return main()
+        except ValueError:
+            print('Not a letter')
+    print('-------------------------------')
+    while turns > 0:
+        print_plank(guess_plank)
+        row, column = get_boat_location()
+        if guess_plank[row][column] == 'x':
+            print('You already guessed that')
+        elif hidden_plank[row][column] == 'k':
+            print('Congratulations, You have hit the battleship')
+            print('Play again :)')
+            guess_plank[row][column] = 'k'
+            break
+        else:
+            print('Sorry, You missed')
+            guess_plank[row][column] = 'x'
+            turns -= 1
+            print('You have ' + str(turns) + ' turns remaining')
+        if turns == 0:
+            print('Sorry, you ran out of turns, The game over')
+            break
 
 
+main()
 
 
