0158827b6c9ae5d7db271a693352d2ce307f0714
shahery <shaheryar924@gmail.com>
2022-03-15 22:27:56 +0000

    Add corrections

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ from random import randint
 # '' for available space
 # x for missing turns
 
-hidden_plank = [['']*6 for x in range(6)]
-guess_plank = [['']*6 for x in range(6)]
+hidden_board = [['']*6 for x in range(6)]
+guess_board = [['']*6 for x in range(6)]
 letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4,
                       'f': 5}
 
 
-def print_plank(plank):
+def print_board(board):
     """
     Return the pathname of the KOS root directory.
     """
@@ -19,45 +19,45 @@ def print_plank(plank):
     print(' a b c d e f')
     print(' -----------')
     row_number = 1
-    for row in plank:
+    for row in board:
         print("%d | %s |" % (row_number, " | ".join(row)))
         row_number += 1
 
 
-def create_boats(plank):
+def create_ships(board):
     """
     Return the pathname of the KOS root directory.
     """
 
-    for boat in range(5):
-        boat_row, boat_column = randint(0, 5), randint(0, 5)
-    while plank[boat_row][boat_column] == 'k':
-        boat_row, boat_column = randint(0, 5), randint(0, 5)
-    plank[boat_row][boat_column] = 'k'
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 5), randint(0, 5)
+    while board[ship_row][ship_column] == 'k':
+        ship_row, ship_column = randint(0, 5), randint(0, 5)
+    board[ship_row][ship_column] = 'k'
 
 
-def get_boat_location():
+def get_ship_location():
     """
     Return the pathname of the KOS root directory.
     """
     print(' -----------')
-    row = input('Please enter a boat row 1-6: ')
+    row = input('Please enter a ship row 1-6: ')
     while row not in '123456':
         print('Please enter a vaild row')
-        row = input('Please  enter a boat row 1-6: ')
-    column = input('Please enter a boat column a-f: ')
+        row = input('Please enter a ship row 1-6: ')
+    column = input('Please enter a ship column a-f: ')
     while column not in 'abcdef':
         print('Please enter a valid column')
-        column = input('Please enter a boat column a-f: ')
+        column = input('Please enter a ship column a-f: ')
     return(int(row)-1, letters_to_numbers[column])
 
 
-def count_hit_boats(plank):
+def count_hit_ships(board):
     """
     Return the pathname of the KOS root directory.
     """
     count = 0
-    for row in plank:
+    for row in board:
         for column in row:
             if column == 'k':
                 count += 1
@@ -75,8 +75,8 @@ def main():
     """
     Return the pathname of the KOS root directory.
     """
-    create_boats(hidden_plank)
-    print(hidden_plank)
+    create_ships(hidden_board)
+    print(hidden_board)
     turns = 5
     print('-------------------------------')
     while True:
@@ -84,22 +84,22 @@ def main():
         if validate_name(name):
             break
         else:
-            print('Name should containt only string characters')
+            print('Name should only contain string characters')
     print(f'Hello {name} welcome to battleship')
     print('-------------------------------')
     while turns > 0:
-        print_plank(guess_plank)
-        row, column = get_boat_location()
-        if guess_plank[row][column] == 'x':
+        print_board(guess_board)
+        row, column = get_ship_location()
+        if guess_board[row][column] == 'x':
             print('You already guessed that')
-        elif hidden_plank[row][column] == 'k':
+        elif hidden_board[row][column] == 'k':
             print('Congratulations, You have hit the target.\n ---You Won---')
             print('Play again :)')
-            guess_plank[row][column] = 'k'
+            guess_board[row][column] = 'k'
             break
         else:
             print('Sorry, You missed')
-            guess_plank[row][column] = 'x'
+            guess_board[row][column] = 'x'
             turns -= 1
             print('You have ' + str(turns) + ' turns remaining')
         if turns == 0:
